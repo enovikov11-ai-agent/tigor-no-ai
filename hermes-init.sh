332c2abf8fb830cf210b9e7fed3c8173d413ce26
Evgenii Novikov <enovikov11@yandex.ru>
2026-08-24 22:28:53 +0200

    init fix

--- hermes-init.sh
+++ hermes-init.sh
@@ -4,7 +4,7 @@ set -Eeuo pipefail
 cd ~
 git clone https://github.com/enovikov11/tigor-ai.git
 
-cd ./tigor-ai
+cd ~/tigor-ai
 git remote remove origin
 git remote add forgejo-push-for-preview http://10.67.69.2:3000/hermes/tigor-ai.git
 git remote add github-pull-and-push-to-main https://github.com/enovikov11/tigor-ai.git
@@ -13,11 +13,12 @@ mkdir -p ~/tigor-ai.worktrees
 
 cp -r ~/hermes-secrets/. ~/tigor-ai/.hermes/
 
+cd ~
 git clone https://github.com/enovikov11/tigor-no-ai.git
 
-mkdir -p tigor-ai.worktrees tigor-no-ai.worktrees
-
-cd ../tigor-no-ai
+cd ~/tigor-no-ai
 git remote remove origin
 git remote add github-pull https://github.com/enovikov11/tigor-no-ai.git
 git remote add github-push-to-feature-branch https://github.com/enovikov11-ai-agent/tigor-no-ai.git
+
+mkdir -p cd ~/tigor-no-ai.worktrees
